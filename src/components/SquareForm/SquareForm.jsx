d7798a979cb48f2a639ea62e77b890c4263fec87
Paige Godfrey <paagodfrey@gmail.com>
2023-07-19 15:02:23 -0700

    update form with state object

--- src/components/SquareForm/SquareForm.jsx
+++ src/components/SquareForm/SquareForm.jsx
@@ -32,39 +32,37 @@ const SquareForm = ({ addSquare, clearSquares }) => {
 
   const sizeOptions = [100, 150, 200];
 
-  const [selectedColor, setSelectedColor] = useState('');
-  const [selectedSize, setSelectedSize] = useState('');
+  const INITIAL_FORM_DATA = { color: '', size: '' }
+  const [formData, setFormData] = useState(INITIAL_FORM_DATA);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     addSquare(
       {
-        color: selectedColor,
-        size: selectedSize,
+        color: formData.color,
+        size: formData.size,
       }
     );
   }
 
-  const handleColorChange = (event) => {
-    setSelectedColor(event.target.value);
-  };
-
-  const handleSizeChange = (event) => {
-    setSelectedSize(event.target.value);
-  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData(fData => ({
+      ...fData,
+      [name]: value
+    }));
+  }
 
   return (
     <div className="SquareForm">
       <form onSubmit={handleSubmit}>
-        <label htmlFor="color">Color:</label>
-        <select onChange={handleColorChange}>
+        <select name="color" onChange={handleChange}>
           <option value={'Select Color'}>-- Select Color --</option>
           {colorOptions.map((color, idx) => (
             <option key={idx} value={color.value}>{color.label}</option>
           ))}
         </select>
-        <label htmlFor="size">Size:</label>
-        <select onChange={handleSizeChange}>
+        <select name="size" onChange={handleChange}>
           <option value={'Select Size'}>-- Select Size --</option>
           {sizeOptions.map((size, idx) => (
             <option key={idx} value={size}>{size}</option>

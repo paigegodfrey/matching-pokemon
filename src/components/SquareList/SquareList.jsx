1b641b12771f5dc1a036ee31c238a9263e757402
Paige Godfrey <paagodfrey@gmail.com>
2023-07-19 16:11:36 -0700

    basic flexbox

--- src/components/SquareList/SquareList.jsx
+++ src/components/SquareList/SquareList.jsx
@@ -4,7 +4,7 @@ import Square from '../Square/Square';
 const SquareList = ({ squares, deleteSquare }) => {
   return (
     <div className='SquareList'>
-      <div className='display-container'>
+      <div className='display-container' style={{display: 'flex', flexWrap: 'wrap'}}>
         {squares.map((square) => (
           <Square
             id={square?.id}

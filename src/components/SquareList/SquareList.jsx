589e040999c780d490657aa1bb95765eaa701b3b
Paige Godfrey <paagodfrey@gmail.com>
2023-07-20 09:19:35 -0700

    fix flexbox

--- src/components/SquareList/SquareList.jsx
+++ src/components/SquareList/SquareList.jsx
@@ -4,6 +4,7 @@ import Square from '../Square/Square';
 const SquareList = ({ squares, deleteSquare }) => {
   return (
     <div className='SquareList'>
+<<<<<<< Updated upstream
       <div className='display-container' style={{display: 'flex', flexWrap: 'wrap'}}>
         {squares.map((square) => (
           <Square
@@ -12,6 +13,20 @@ const SquareList = ({ squares, deleteSquare }) => {
             backgroundColor={square?.color}
             size={square?.size}
             key={square?.id} />
+=======
+      <div>
+        <button className='reset-button' onClick={clearSquares}>
+          Clear All
+        </button>
+      </div>
+      <div className='display-container' style={{ display: 'flex', flexWrap: 'wrap' }}>
+        {squares.map((square) => (
+          <Square
+            square={square}
+            duplicateSquare={duplicateSquare}
+            deleteSquare={deleteSquare}
+            key={square.id} />
+>>>>>>> Stashed changes
         ))}
       </div>
     </div>
